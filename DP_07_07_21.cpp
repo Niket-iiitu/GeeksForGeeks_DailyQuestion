ed28f1e64acbfe6e26e8a820df18a7965941d6b7
Niket-iiitu <niketagrawal777@gmail.com>
2021-07-07 11:32:09 +0530

    7 July

---
+++ DP_07_07_21.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define modulo 1000000007
+
+class Solution
+{
+    public:
+    pair<long,long> indexes(vector<long long> v, long long x)
+    {
+        // code here
+    }
+};
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+    return 0;
+}
